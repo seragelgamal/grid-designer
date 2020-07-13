298d37eef1c9eced2c0b54321ea9aaa9854ab8ce
seragelgamal <55516965+seragelgamal@users.noreply.github.com>
2020-07-13 10:35:21 -0600

    commit

--- main.js
+++ main.js
@@ -1,2 +1,55 @@
 // TITLE
+const numberOfRows = 10;
+const numberOfColumns = 10;
+let container = document.getElementById('container');
+let colourSelector = document.getElementById('colourSelector');
+let grid = createGridArray();
+let player = {
+    row: 0,
+    column: 0
+}
+createDivGrid(grid);
 
+
+
+function createGridArray() {
+    return [ [0, 0, 0, 0, 0, 0, 0, 0, 0, 0], 
+             [0, 0, 0, 0, 0, 0, 0, 0, 0, 0], 
+             [0, 0, 0, 0, 0, 0, 0, 0, 0, 0], 
+             [0, 0, 0, 0, 0, 0, 0, 0, 0, 0], 
+             [0, 0, 0, 0, 0, 0, 0, 0, 0, 0], 
+             [0, 0, 0, 2, 0, 0, 0, 0, 0, 0], 
+             [0, 0, 0, 0, 0, 0, 0, 0, 0, 0], 
+             [0, 0, 0, 0, 0, 0, 0, 0, 0, 0], 
+             [0, 0, 0, 0, 0, 0, 0, 0, 0, 0], 
+             [0, 0, 0, 0, 0, 0, 0, 0, 0, 0], ]
+}
+
+function createDivGrid(grid) {
+    for (let row = 0; row < numberOfRows; row++) {
+        for (let column = 0; column < numberOfColumns; column++) {
+            let divElement = document.createElement('div');
+            divElement.dataset.row = row;
+            divElement.dataset.column = column;
+            divElement.addEventListener('click', changeColour);
+            if (grid[row][column] == 1) {
+                divElement.classList.add('grey');
+            } else if (grid[row][column] == 2) {
+                divElement.classList.add('player');
+                player.row = divElement.dataset.row;
+                player.column = divElement.dataset.column;
+            }
+            container.append(divElement);
+        }
+    }
+}
+
+function changeColour(e) {
+    if (colourSelector.value == 'grey') {
+        e.target.classList.add('grey');
+        grid[e.target.dataset.row][e.target.dataset.column] = 1;
+    } else if (colourSelector.value == 'white') {
+        e.target.classList.remove('grey');
+        grid[e.target.dataset.row][e.target.dataset.column] = 0;
+    }
+}
